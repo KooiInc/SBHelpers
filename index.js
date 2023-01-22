8f765991a64ec3b1aedd43c264fd2fbce8df86c0
Renzo Kooi <renzo.kooi@gmail.com>
2023-01-22 14:32:47 +0100

    Version and b&b

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import $ from "jqlmodule";
-export {logFactory}
+defaultStyling();
+export { $, logFactory, defaultStyling };
+
 function logFactory() {
-  const ul = $(`<ul>`);
+  const ul = $(`<ul/>`);
   const head = t => `${t}`.startsWith(`!!`) ? ` class="head"` : ``;
   const logItem = top => t => ul.append(
     `<li${head(t)}>${`${t}`.replace(/^!!/, ``)}</li>`,
@@ -9,4 +11,38 @@ function logFactory() {
   return {
     log: (...txt) => txt.forEach( logItem() ),
     logTop: (...txt) => txt.forEach( logItem(true) ), };
-};
+}
+
+function defaultStyling() {
+  [
+    [`body`, {font: `normal 14px/17px verdana, arial`, margin: `1rem`,}],
+    [`code`, {
+      color: `green`,
+      backgroundColor: `#eee`,
+      padding: `3px`,
+      fontFamily: `'Courier New', Courier, monospace`,
+    }],
+    [`code.codeblock`, {display: `block`, padding: `6px`, border: `1px solid #999`, margin: `0.5rem 0`}],
+    [`h3`, {marginTop: `1.5rem`}],
+    [`.thickBorder`, {border: `5px solid green`, borderWidth: `5px`, padding: `0.5rem`, display: `inline-block`}],
+    ["a.ExternalLink", {
+      textDecoration: `none`,
+      color: `blue`,
+      backgroundColor: `#EEE`,
+      padding: `3px`,
+      'font-weight': `bold`
+    }],
+    [`.cmmt`, {color: `#888`}],
+    [`.hidden`, {display: `none`}],
+    [`.attention`, {color: `red`, fontSize: `1.2em`, fontWeight: `bold`}],
+    [`li`, { listStyle: `'\\2713'`, paddingLeft: `6px`, margin: `0.5rem 0 0 -1.2rem`, fontFamily: `monospace` }],
+    [`li.head`, {
+      listStyleType: `none`,
+      fontWeight: `bold`,
+      marginTop: `0.8rem`,
+      marginBottom: `-0.2rem`,
+      fontFamily: `revert`
+    }],
+    [`.err`, {fontStyle: `italic`, color: `red`}]
+  ].forEach( ( styleDeclaration ) => $.setStyle(...styleDeclaration) );
+}
