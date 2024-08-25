a9c25275e7e8b9c11e26011abcd4e7c66c33dec2
Renzo Kooi <renzo.kooi@gmail.com>
2024-08-25 15:10:44 +0200

    Conditial (stackblitz) info fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export { $, logFactory, regexhelper, xDate, dtFormat, dtDiffCalc, extendSymbolic
 
 function fixSBLinks2TopProblem() {
   // fix for stackblitz rewriting hrefs with target _top (see README)
-  /stackblitz/i.test(top.location.href) &&
+  /stackblitz/i.test(location.href) &&
     console.info(`✔ Stackblitz rewrites links to _top. The 'stackblitzhelpers' module fixed it.`);
   document.addEventListener(`click`, evt => {
     if (evt.target.href) {
